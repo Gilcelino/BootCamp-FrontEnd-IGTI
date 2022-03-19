1668f594fffc7459272562191f0226673ee7f59e
Gilcelino <gilcelino@atlantasistemas.com.br>
2022-03-19 19:07:47 -0300

    Trabalhando com dados no componente de seleção

--- Exercício Prático - Modulo 2/delicias/src/app/componentes/selecao/selecao.component.ts
+++ Exercício Prático - Modulo 2/delicias/src/app/componentes/selecao/selecao.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { CheckboxControlValueAccessor } from '@angular/forms';
 @Component({
   selector: 'app-selecao',
   templateUrl: './selecao.component.html',
@@ -10,7 +10,27 @@ export class SelecaoComponent implements OnInit {
   @Input() opcoes: String[] = [];
   @Input() escolhaAte: number = 1;
 
+  opcoesSelecionadas: { index: number; item: string }[] = [];
+
+  objetoHabilitado: boolean = true;
   constructor() {}
 
   ngOnInit(): void {}
+
+  onChange(i: number, value: boolean) {
+    const item = { index: i, item: this.opcoes[i].toString() };
+
+    if (value) {
+      this.opcoesSelecionadas.push(item);
+    } else {
+      this.opcoesSelecionadas.splice(this.retornaIndexItemSelecionado(item), 1);
+    }
+
+    this.objetoHabilitado = this.opcoesSelecionadas.length < this.escolhaAte;
+  }
+
+  retornaIndexItemSelecionado = (item: any) =>
+    this.opcoesSelecionadas.findIndex(
+      (user, index, array) => user.index === item.index
+    );
 }
